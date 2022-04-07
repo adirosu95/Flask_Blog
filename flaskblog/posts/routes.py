a129b4b5172aea7ad9e327c61a2fb38c3bc75211
Adrian Marin Rosu <adrian.marin.rosu@ericsson.com>
2022-04-07 15:48:33 +0300

    initial commit

---
+++ flaskblog/posts/routes.py
@@ -0,0 +1,73 @@
+from flask import Blueprint
+from flask import render_template, flash, redirect, url_for, request, abort
+from flaskblog import db
+from flaskblog.posts.forms import PostForm
+from flaskblog.models import Post
+from flask_login import current_user, login_required
+
+posts = Blueprint('posts', __name__)
+
+
+@posts.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.home'))
+    context = {
+        'title': 'New Post',
+        'form': form,
+        'legend': 'New Post'
+    }
+    return render_template('create_post.html', **context)
+
+
+@posts.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    context = {
+        'title': post.title,
+        'post': post
+    }
+    return render_template('post.html', **context)
+
+
+@posts.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    context = {
+        'title': 'Update Post',
+        'form': form,
+        'legend': 'Update Post'
+    }
+    return render_template('create_post.html', **context)
+
+
+@posts.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('main.home'))
+
